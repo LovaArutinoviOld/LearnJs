f14809b97f7ee29a306162d8459e116c88deb170
LovaArutinoviW <arutinovl@lemondo.com>
2022-01-26 10:49:52 +0400

    fix weather

--- JavaScript v2.0/Практика по массивам - получаем прогноз погоды по API/script.js
+++ JavaScript v2.0/Практика по массивам - получаем прогноз погоды по API/script.js
@@ -1,9 +1,19 @@
-fetch('https://api.openweathermap.org/data/2.5/forecast/weather?id=703448&appid=a24b4f99724dd1616520551f0bd230ff')
-		.then(function (resp) { return resp.json() })// Convert data to json
-		.then(function(data) {
-			console.log(data);
-		})
-		.catch(function () {
-			// catch any errors
-		});
-		https://samples.openweathermap.org/data/2.5/forecast/hourly?q=M%C3%BCnchen,DE&appid=439d4b804bc8187953eb36d2a8c26a02
+let city = document.querySelector('.weather__title');
+let deg = document.querySelector('.weather__count');
+let weatherWord = document.querySelector('.weather__disclaimer');
+let weatherFeatures = document.querySelector('.weather__features');
+
+fetch('https://api.openweathermap.org/data/2.5/weather?id=1282028&appid=4b24ebc0c6ce204e57db7e4cf831c8cd')
+.then(function(resp) {return resp.json() }) // convert data to json
+.then(function(data){
+	console.log(data);
+	city.textContent = data.name
+	deg.innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+	weatherWord.textContent = data.weather[0].description
+	weatherFeatures.querySelector('li').innerHTML = 
+	`<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`
+})
+.catch(function(){
+	// catch any errors
+}) 
+// https://openweathermap.org/img/wn/13d@2x.png
